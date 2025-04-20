b7df514a6274940c6424530dbe5c436192687dd9
premg513 <gunduboinaprem@gmail.com>
2025-04-20 15:59:45 +0530

    Contact Manager Application Done!

--- server/app.js
+++ server/app.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //cors
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://contact-manager-8zfg.vercel.app",
+  })
+);
 // Middleware
 app.use(express.json());
 
